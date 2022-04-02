50b9da0b258edb60076843c78743d8d07e02da6e
Ivanran225 <ivanr.an225@gmail.com>
2022-04-02 16:37:41 -0300

    Finalizado

---
+++ Assets/Scripts/MayorDeEdad.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MayorDeEdad : MonoBehaviour
+{
+    public int IngresoTexto;
+    public Text textoCartelito;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (IngresoTexto >= 18)
+        {
+
+            textoCartelito.text = "Sos mayor de edad";
+            Debug.Log("Sos mayor de edad");
+
+        }
+        else
+        {
+
+            textoCartelito.text = "Sos menor de edad";
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/contr.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class contr : MonoBehaviour
+{
+    string contraok = "123";
+    string contra;
+    public Text ingresocontra;
+    public GameObject Cartel;
+    public Text tmensaje;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cartel.SetActive(false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void contrasenia()
+    {
+
+        contra = ingresocontra.text;
+
+        if(contra == contraok)
+        {
+            Debug.Log("Bienvenido");
+            tmensaje.text = "bienvenido";
+            Cartel.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("Incorecto");
+            tmensaje.text = "incorecto";
+           Cartel.SetActive(true);
+        }
+    }
+}
